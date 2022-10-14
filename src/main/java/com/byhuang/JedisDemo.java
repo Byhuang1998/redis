7b8cc92ec0e36584c7a72ae9b2c0d1313377f5ae
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-14 14:19:46 +0800

    java links redis server running in ubuntu sys

---
+++ src/main/java/com/byhuang/JedisDemo.java
@@ -0,0 +1,18 @@
+package com.byhuang;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2022/10/14 10:47
+ * @description TODO
+ */
+public class JedisDemo {
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.119.130", 6379);
+        String ping = jedis.ping();
+        System.out.println(ping);
+    }
+}
